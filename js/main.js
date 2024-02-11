0087fde603d85755cab235aaa7f9c607981ff8b9
MathieuRostaing <121859783+MathieuRostaing@users.noreply.github.com>
2024-02-11 12:44:49 +0000

    trad et rectification

--- js/main.js
+++ js/main.js
@@ -1,28 +1,3 @@
-// projet avorte d une nav qui illuminait la partie de la page dans laquelle on est
-// let mainNavLinks = document.querySelectorAll("nav ul li a");
-// let mainSections = document.querySelectorAll("part");
-
-// let lastId;
-// let cur = [];
-
-// window.addEventListener("scroll", event => {
-//   let fromTop = window.scrollY;
-
-//   mainNavLinks.forEach(link => {
-//     let section = document.querySelector(link.hash);
-
-//     if (
-//       section.offsetTop-1 <= fromTop &&
-//       section.offsetTop + section.offsetHeight > fromTop
-//     ) {
-//       link.classList.add("current");
-//     } else {
-//       link.classList.remove("current");
-//     }
-//   });
-// });
-
-
 // Pour l animation au defilement
 function reveal() {
     var reveals = document.querySelectorAll(".reveal"); //on recupere les elements pour lesquels ca s applique
